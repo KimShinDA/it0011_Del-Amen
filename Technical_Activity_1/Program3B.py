11426397c67d9ce45dcf65551153793e65d8141c
KimShinDA <delamenkimshin@gmail.com>
2025-02-01 23:44:27 +0800

    Update output without 2 and 4 02/01/25 2344

--- Technical_Activity_1/Program3B.py
+++ Technical_Activity_1/Program3B.py
@@ -1,6 +1,6 @@
 rows = 1
 while rows <= 7:
-    if rows != 2 and rows != 4:  
+    if rows != 2 and rows != 4:  #displaying 1 to 7 except for rows 2 and 4
         col = 1
         while col <= rows:
             print(rows, end="")
