01203200a42947422f5dcaf6a319990e9b2851de
KimShinDA <delamenkimshin@gmail.com>
2025-02-28 12:41:10 +0800

    Act 4B 1240 022825

--- Activity_4B/Program_2_Dictionary.py
+++ Activity_4B/Program_2_Dictionary.py
@@ -1,25 +1,25 @@
-exchange_rates = {}  # Dictionary to store rates
-currency_names = {}  # Dictionary to store currency names
+exchangeRates = {}  # Dictionary to store rates
+currencyNames = {}  # Dictionary to store currency names
 
-with open("currency.csv", "r", encoding="ISO-8859-1") as file:
+with open("currency.csv", "r") as file:
     next(file)  
     for line in file:
         parts = line.strip().split(",")  # Split CSV line by comma
-        currency_code = parts[0].strip()
-        currency_name = parts[1].strip()
-        exchange_rate = float(parts[2].strip())
+        currencyCode = parts[0].strip()
+        currencyName = parts[1].strip()
+        exchangeRate = float(parts[2].strip())
 
-        exchange_rates[currency_code] = exchange_rate
-        currency_names[currency_code] = currency_name  
+        exchangeRates[currencyCode] = exchangeRate
+        currencyNames[currencyCode] = currencyName  
 
 # User input
-usd_amount = float(input("How much dollar do you have? "))
-target_currency = input("What currency you want to have? ").strip().upper()
+usdAmount = float(input("How much dollar do you have? "))
+currencyChoice = input("What currency you want to have? ").strip().upper() # Convert to upper case
 
 # Convert and display result
-if target_currency in exchange_rates:
-    converted_amount = usd_amount * exchange_rates[target_currency]
-    print(f"\nDollar: {usd_amount} USD")
-    print(f"{currency_names[target_currency]}: {converted_amount:.6f}") 
+if currencyChoice in exchangeRates:
+    convertedAmount = usdAmount * exchangeRates[currencyChoice]
+    print(f"\nDollar: {usdAmount} USD")
+    print(f"{currencyNames[currencyChoice]}: {convertedAmount:.6f}") 
 else:
     print("Currency not found.")
